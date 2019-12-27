8db3dbaa6cd6ef509f1730e73f47146b0363cb99
Ricardo Quinteiro <34577049+RicardoQuinteiro@users.noreply.github.com>
2019-12-27 01:13:23 +0000

    Update analysis_wheat.R

--- analysis_wheat.R
+++ analysis_wheat.R
@@ -2,3 +2,21 @@ seeds_dataset = read.delim("~/GitHub/ProjectAM/seeds_dataset.txt")
 
 #summary of the data
 summary(seeds_dataset)
+
+
+#Preliminary Analysis (Frank) --- Standardization, Plots...
+
+
+#Decision Tree
+
+
+#SVM/LDA
+
+
+#KNN
+
+
+#PCA (Pedro)
+
+
+#Clustering (Ricardo) 3 clusters
